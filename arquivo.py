6c82ce710fa2adec7ab0b3e8690a3addeb76adda
Henrique Coelho Vargas <henriquecoelho.vargas@gmail.com>
2023-03-16 16:45:15 -0300

    Criei uma programação para calcular a idade de alguém que nasceu em 1997 e de uma kid (criança)

---
+++ arquivo.py
@@ -0,0 +1,16 @@
+Nascimento = 1997
+DataAtual = 2023
+NascimentoKid = 2012
+
+def calculoIdade(Nascimento,DataAtual):
+    idade = DataAtual - Nascimento
+    return idade
+
+Idade = calculoIdade (Nascimento,DataAtual)
+IdadeKid = calculoIdade (NascimentoKid,DataAtual)
+
+print (' sua idade é '+ str(Idade))
+print ('a idade da kid é '+str(IdadeKid))
+
+
+
